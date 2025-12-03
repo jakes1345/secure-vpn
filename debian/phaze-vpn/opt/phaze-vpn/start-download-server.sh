b3ec00ce3edf66cc65622e7a055ddae51e62f94a
PhazeVPN <phazevpn@example.com>
2025-12-03 13:36:45 -0600

    Clean up repository - remove junk files and keep only essential code

--- debian/phaze-vpn/opt/phaze-vpn/start-download-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start the client download server
-
-cd "$(dirname "$0")"
-python3 client-download-server.py
-
--- debian/phaze-vpn/opt/phaze-vpn/start-vpn-gui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start VPN GUI Application
-
-cd "$(dirname "$0")"
-python3 vpn-gui.py
-
